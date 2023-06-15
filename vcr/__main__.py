bd9eb65a5d920d2b91a5293eafbb4c2444f93d02
Lee Smet <lee.smet@hotmail.com>
2023-06-15 11:20:07 +0200

    Minor code restructure
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- vcr/__main__.py
+++ vcr/__main__.py
@@ -6,37 +6,50 @@ import speech_recognition as sr
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
 
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+def main():
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
-r = sr.Recognizer()
-m = sr.Microphone()
+    nlu_engine = _setup_engine()
 
-# Load NLU dataset and fit in model
-dataset = Dataset.from_yaml_files("en", ["training_dataset/model.yaml"])
-print("Loaded dataset:\r\n\t{}".format(dataset.json))
-nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
-nlu_engine = nlu_engine.fit(dataset)
+    # Setup audio input and recognizer
+    r = sr.Recognizer()
+    m = sr.Microphone()
 
-
-try:
-    print("Calibrating microphone")
-    with m as source: r.adjust_for_ambient_noise(source)
-    print("Set minimum energy treshold to {}".format(r.energy_threshold))
-    if r.energy_threshold < 300:
-        r.energy_threshold = 300
+    try:
+        print("Calibrating microphone")
+        with m as source: r.adjust_for_ambient_noise(source)
         print("Set minimum energy treshold to {}".format(r.energy_threshold))
-    while True:
-        print("Say something")
-        with m as source: audio = r.listen(source)
-        print("Captured input")
-        try:
-            # We just care about english for now, also use the small dataset instead of the base, which requires about 2GB of ram
-            value = r.recognize_whisper(audio, model="small.en", language="english")
-            print("Recognized input:\r\n{}".format(value))
-            parsing = nlu_engine.parse(value)
-            print(parsing)
-        except sr.UnknownValueError:
-            print("I did not understand what you are saying")
-except KeyboardInterrupt:
-    pass
+        if r.energy_threshold < 300:
+            r.energy_threshold = 300
+            print("Set minimum energy treshold to {}".format(r.energy_threshold))
+        while True:
+            print("Say something")
+            with m as source: audio = r.listen(source)
+            print("Captured input")
+            try:
+                # We just care about english for now, also use the small dataset instead of the base, which requires about 2GB of ram
+                value = r.recognize_whisper(audio, model="small.en", language="english")
+                print("Recognized input:\r\n{}".format(value))
+                parsing = _extract_intent(value, nlu_engine)
+                print(parsing)
+            except sr.UnknownValueError:
+                print("I did not understand what you are saying")
+    except KeyboardInterrupt:
+        pass
+
+def _setup_engine():
+    # Load NLU dataset and fit in model
+    dataset = Dataset.from_yaml_files("en", ["training_dataset/model.yaml"])
+    print("Loaded dataset:\r\n\t{}".format(dataset.json))
+    nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
+    nlu_engine = nlu_engine.fit(dataset)
+    return nlu_engine
+
+def _extract_intent(input: str, engine: SnipsNLUEngine):
+    print("Analyzing input to figure out what you mean")
+    return engine.parse(input)
+
+if __name__ == "__main__":
+    main()
+
